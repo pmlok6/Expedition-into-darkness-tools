5c4361f24fceddb0fbca7bc81e6b69826a750e11
pmlok6 <65672699+pmlok6@users.noreply.github.com>
2026-07-27 00:05:01 +0200

    Update wiki-api.js

--- js/wiki-api.js
+++ js/wiki-api.js
@@ -1,1 +1,86 @@
+const API =
+"https://expeditionintodarkness.wiki.gg/api.php";
 
+
+
+export async function invoke(
+    module,
+    functionName
+)
+{
+
+    const params =
+        new URLSearchParams({
+
+            action: "expandtemplates",
+
+            text:
+            `{{#invoke:${module}|${functionName}}}`,
+
+            prop: "wikitext",
+
+            format: "json",
+
+            origin: "*"
+
+        });
+
+
+
+    const response =
+        await fetch(
+            API + "?" + params.toString()
+        );
+
+
+
+    if(!response.ok)
+    {
+        throw new Error(
+            "Wiki API error : " +
+            response.status
+        );
+    }
+
+
+
+    const data =
+        await response.json();
+
+
+
+    if(
+        !data.expandtemplates ||
+        !data.expandtemplates.wikitext
+    )
+    {
+        throw new Error(
+            "No data returned from wiki"
+        );
+    }
+
+
+
+    try
+    {
+
+        return JSON.parse(
+            data.expandtemplates.wikitext
+        );
+
+    }
+
+    catch(error)
+    {
+
+        console.error(
+            "JSON parsing error",
+            data.expandtemplates.wikitext
+        );
+
+
+        throw error;
+
+    }
+
+}
