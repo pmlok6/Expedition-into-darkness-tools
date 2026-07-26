01be57eaa3b482f751b2a46cb569d11fd19ce932
pmlok6 <65672699+pmlok6@users.noreply.github.com>
2026-07-27 00:24:38 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
 console.log("Expedition Tools loaded");
+
+document.addEventListener(
+"DOMContentLoaded",
+function()
+{
+
+    const nav =
+        document.getElementById(
+            "main-navigation"
+        );
+
+
+    if(!nav)
+    {
+        return;
+    }
+
+
+    nav.innerHTML = `
+
+    <a href="../index.html">
+        Home
+    </a>
+
+    <a href="../database.html">
+        Database
+    </a>
+
+    <a href="armor.html">
+        Armor Calculator
+    </a>
+
+    <a href="weapon.html">
+        Weapon Calculator
+    </a>
+
+    <a href="../leveling.html">
+        Leveling
+    </a>
+
+    <a href="../maps.html">
+        Maps
+    </a>
+
+    `;
+
+});
