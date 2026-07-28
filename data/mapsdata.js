b91eae9b12bbea951427447ed199864f0bb4f66b
pmlok6 <65672699+pmlok6@users.noreply.github.com>
2026-07-28 21:29:15 +0200

    Update mapsdata.js

--- data/mapsdata.js
+++ data/mapsdata.js
@@ -1,14 +1,27 @@
-export default {
+const MAPS={
 
     catacomb:{
         name:"Catacomb",
-        floors:[
-            {id:2,label:"+2",image:"../assets/maps/catacomb/floor2.webp"},
-            {id:1,label:"+1",image:"../assets/maps/catacomb/floor1.webp"},
-            {id:0,label:"0",image:"../assets/maps/catacomb/floor0.webp"},
-            {id:-1,label:"-1",image:"../assets/maps/catacomb/floor-1.webp"},
-            {id:-2,label:"-2",image:"../assets/maps/catacomb/floor-2.webp"}
+        floors:
+        [
+            {
+                id:1,
+                label:"+1",
+                image:"../assets/catacomb/floor1.png"
+            },
+            {
+                id:0,
+                label:"0",
+                image:"../assets/catacomb/floor0.png"
+            },
+            {
+                id:-1,
+                label:"-1",
+                image:"../assets/catacomb/floor-1.png"
+            },            
         ]
     }
 
 };
+
+export default MAPS;
