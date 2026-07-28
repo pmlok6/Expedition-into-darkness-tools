193d9b653e611833e6fb339348a3c8c6fbe9f0b3
pmlok6 <65672699+pmlok6@users.noreply.github.com>
2026-07-28 21:33:52 +0200

    Update mapsdata.js

--- data/mapsdata.js
+++ data/mapsdata.js
@@ -2,23 +2,19 @@ const MAPS={
 
     catacomb:{
         name:"Catacomb",
-        floors:
-        [
-            {
-                id:1,
-                label:"+1",
-                image:"../assets/catacomb/floor1.png"
-            },
-            {
-                id:0,
-                label:"0",
-                image:"../assets/catacomb/floor0.png"
-            },
-            {
-                id:-1,
-                label:"-1",
-                image:"../assets/catacomb/floor-1.png"
-            },            
+        floors:[
+            {id:2,label:"+2",image:"../assets/catacomb/floor2.png"},
+            {id:1,label:"+1",image:"../assets/catacomb/floor1.png"},
+            {id:0,label:"0",image:"../assets/catacomb/floor0.png"},
+            {id:-1,label:"-1",image:"../assets/catacomb/floor-1.png"},
+            {id:-2,label:"-2",image:"../assets/catacomb/floor-2.png"}
+        ]
+    },
+
+    moor:{
+        name:"Moor",
+        floors:[
+            {id:0,label:"0",image:"../assets/moor/floor0.png"}
         ]
     }
 
