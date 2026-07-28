d24d5325bcd027bc091d4058e28f4525943bf5d9
pmlok6 <65672699+pmlok6@users.noreply.github.com>
2026-07-28 21:21:01 +0200

    Create mapsdata.js for catacomb information
    Added mapsdata.js with catacomb floor data.

---
+++ data/mapsdata.js
@@ -0,0 +1,14 @@
+export default {
+
+    catacomb:{
+        name:"Catacomb",
+        floors:[
+            {id:2,label:"+2",image:"../assets/maps/catacomb/floor2.webp"},
+            {id:1,label:"+1",image:"../assets/maps/catacomb/floor1.webp"},
+            {id:0,label:"0",image:"../assets/maps/catacomb/floor0.webp"},
+            {id:-1,label:"-1",image:"../assets/maps/catacomb/floor-1.webp"},
+            {id:-2,label:"-2",image:"../assets/maps/catacomb/floor-2.webp"}
+        ]
+    }
+
+};
