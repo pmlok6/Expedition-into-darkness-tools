14d1730f305232e731a2f67566619676d5cf2a02
pmlok6 <65672699+pmlok6@users.noreply.github.com>
2026-07-30 00:12:57 +0200

    Update maps.js

--- maps/maps.js
+++ maps/maps.js
@@ -25,6 +25,8 @@ const selector=document.getElementById("map-selector");
 const image=document.getElementById("map-image");
 const elevator=document.getElementById("map-elevator");
 const markerMenu=document.getElementById("marker-menu");
+const markerMenu=document.getElementById("marker-menu");
+const legend=document.getElementById("map-legend");
 
 let MAPS=[];
 let currentMap=null;
