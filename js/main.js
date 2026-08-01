7c21729b7750e331d930a5ec9506693dcd291fbb
pmlok6 <65672699+pmlok6@users.noreply.github.com>
2026-08-01 14:33:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 
    Features:
    - Navigation
+   - Footer
    - Auth state
 ========================================== */
 
@@ -12,12 +13,14 @@ import { supabase } from "./supabase.js";
 // ===============================
 // navigation
 // ===============================
+
 function loadNavigation()
 {
     const nav=document.getElementById("main-navigation");
 
     if(!nav)
         return;
+
 
     const root=
     location.pathname.includes("/maps/")||
@@ -33,29 +36,106 @@ function loadNavigation()
     nav.innerHTML=
 `
 <div class="nav-logo">
+
     <a href="${root}index.html">
+
         <img src="${root}assets/Site-logo.png">
+
     </a>
+
 </div>
 
+
 <div class="nav-links">
-    <a href="${root}maps/maps.html">🗺 Maps</a>
-    <a href="${root}skills/skills.html">🌳 Skill Tree</a>
-    <a href="${root}leveling/leveling.html">📈 Leveling</a>
-    <a href="${root}calculators/armor.html">🛡 Armor</a>
-    <a href="${root}calculators/weapon.html">⚔ Weapon</a>
-    <a href="${root}login.html">🔑 Login</a>
+
+    <a href="${root}maps/maps.html">
+        🗺 Maps
+    </a>
+
+    <a href="${root}skills/skills.html">
+        🌳 Skill Tree
+    </a>
+
+    <a href="${root}leveling/leveling.html">
+        📈 Leveling
+    </a>
+
+    <a href="${root}calculators/armor.html">
+        🛡 Armor
+    </a>
+
+    <a href="${root}calculators/weapon.html">
+        ⚔ Weapon
+    </a>
+
+    <a href="${root}login.html">
+        🔑 Login
+    </a>
+
 </div>
 `;
 }
+
+
+// ===============================
+// footer
+// ===============================
+
+function loadFooter()
+{
+    const footer=document.getElementById("site-footer");
+
+    if(!footer)
+        return;
+
+
+    const root=
+    location.pathname.includes("/maps/")||
+    location.pathname.includes("/skills/")||
+    location.pathname.includes("/leveling/")||
+    location.pathname.includes("/calculators/")
+    ?
+    "../"
+    :
+    "./";
+
+
+    footer.innerHTML=
+`
+<div class="footer-studio">
+
+    <a href="${root}studio.html">
+
+        <img 
+            src="${root}assets/antediluvian-logo.png"
+            alt="Antediluvian Interactive">
+
+    </a>
+
+</div>
+
+
+<div class="footer-credit">
+
+    <a href="${root}credits.html">
+        Credits
+    </a>
+
+</div>
+`;
+}
+
+
 // ===============================
 // auth
 // ===============================
+
 async function checkUser()
 {
     const {data}=await supabase.auth.getSession();
 
     const user=data.session?.user??null;
+
 
     console.log(
         user?
@@ -69,8 +149,14 @@ async function checkUser()
 // ===============================
 // start
 // ===============================
-document.addEventListener("DOMContentLoaded",()=>
+
+document.addEventListener(
+"DOMContentLoaded",
+()=>
 {
     loadNavigation();
+
+    loadFooter();
+
     checkUser();
 });
